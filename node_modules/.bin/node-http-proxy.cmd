6971e0edef007eadc1dba1019d8e91f62413fb54
Red1Reaper <aitor.rosell.torralba@gmail.com>
2018-12-11 10:20:30 +0100

    build info

--- node_modules/.bin/node-http-proxy.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\http-proxy\bin\node-http-proxy" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\http-proxy\bin\node-http-proxy" %*
-)
