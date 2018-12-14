05d8395767d392ae2a84526b78a7542aa4514637
Red1Reaper <aitor.rosell.torralba@gmail.com>
2018-12-14 12:22:26 +0100

    update

--- script/options.js
+++ script/options.js
@@ -12,7 +12,11 @@ const dns = require('dns')
 var Q = require('q');
 const { remote } = require('electron')
 const path = require('path')
+var elerem = require('electron').remote;
+var dialog = elerem.dialog;
+var app = elerem.app;
 
+var http = require('http');
 var serverListFile
 
 if(config.has('serverList')){
@@ -30,8 +34,56 @@ var inititalization = function(){
 	});
 } 
 
+
+function myUrlSaveAsComplete(err){
+    alert("done");
+}
+
+
+function download (url, dest, cb) {
+    var file = fs.createWriteStream(dest);
+    var request = http.get(url, function(response) {
+        response.pipe(file);
+        file.on('finish', function() {
+            file.close(cb); // close() is async, call cb after close completes.
+        });
+    }).on('error', function(err) { // Handle errors
+        fs.unlink(dest); // Delete the file async. (But we don't check the result)
+        if (cb) cb(err.message);
+    });
+};
+
+var exportServerList = function(){
+	var data = {
+		serverlist: []
+	};
+	data.serverlist = serverList;
+	var json = JSON.stringify(data);
+	var toLocalPath = path.resolve(app.getPath("desktop")) 
+    var userChosenPath = dialog.showSaveDialog({ defaultPath: toLocalPath+"/Server List.json",title: 'Server List',filters: [{
+      name: 'Server List',
+      extensions: ['json']
+    }]
+  });
+    if(userChosenPath){
+        fs.writeFile(userChosenPath, json, function(err) {
+    // file saved or err
+});
+    }
+	//myUrlSaveAs(url)
+	
+}
+
 var importServerList = function(){ 
-   $('#importFile').click()
+   //$('#importFile').click()
+    dialog.showOpenDialog({
+        properties: ['openFile', 'multiSelections']
+    }, function (files) {
+        if (files !== undefined) {
+            // handle files
+			serverListFile = files
+        }
+    });
 }
 
 var returnToIndex= function(){
