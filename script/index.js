5ef783ab531592d91ee8eb1ba5334279afc01efc
Red1Reaper <aitor.rosell.torralba@gmail.com>
2018-12-15 17:52:05 +0100

    Update index.js

--- script/index.js
+++ script/index.js
@@ -50,14 +50,21 @@ var openServer = function(server){
 	}else{
 		var fakeInternet = ""
 	}
+	if(broadcastEnabled){
+		var broadCast = " --broadcast"
+	} else{
+		var broadCast = ""
+	}
+	var pmtuCommand = " --pmtu "+pmtu
+	var netIf = " --netif "+networkInterface
 	if (OS == "win32"){ //If OS is Windows
 		if (os.arch == "x64"){ //win64
-			var commandString = "start cmd.exe /K "+lanPlayLocation+fakeInternet+" --relay-server-addr "+ server
+			var commandString = "start cmd.exe /K "+lanPlayLocation+fakeInternet+broadCast+pmtuComand+netIf+" --relay-server-addr "+ server
 		} else { //win32
-			var commandString = "start cmd.exe /K "+lanPlayLocation+fakeInternet+" --relay-server-addr "+ server
+			var commandString = "start cmd.exe /K "+lanPlayLocation+fakeInternet+broadCast+pmtuComand+netIf+" --relay-server-addr "+ server
 		}
 	} else if(OS == "linux"){ //If OS is Linux
-		var commandString = "x-terminal-emulator -e "+lanPlayLocation+fakeInternet+" --relay-server-addr "+ server
+		var commandString = "x-terminal-emulator -e "+lanPlayLocation+fakeInternet+broadCast+pmtuComand+netIf+" --relay-server-addr "+ server
 	} else if (OS == "darwin"){//If OS is MacOS
 		var commandString = "open -n ./lan-play-macos --args"+fakeInternet+" --relay-server-addr "+ server
 	} else {
